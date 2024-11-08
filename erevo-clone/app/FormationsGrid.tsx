9b9a8243bd01eddbe21f95cb5d2a361ee974391c
Bouwerqrq Nass <bouwerqrqnass@MacBook-Pro-de-Bouwerqrq.local>
2024-11-08 16:47:45 +0100

    upd

--- erevo-clone/app/FormationsGrid.tsx
+++ erevo-clone/app/FormationsGrid.tsx
@@ -16,8 +16,13 @@ export default function FormationsGrid() {
 ];
 
   return (
-    <div className="container mx-auto py-8 px-4 mt-24 mb-16">
-        <h2 className="text-3xl text-[#D88941] text-center text-6xl pb-16 font-bold text-white mb-2">Choisissez votre formation</h2>
+    <div className="container mx-auto py-8  mt-24 mb-16">
+        <div className="bg-[#FDF5EB] py-8 mb-8 rounded-lg">
+          <h2 className="text-5xl font-bold text-center text-[#2A394A] mb-4 leading-tight">
+            Choisissez votre <span className="text-[#C97435]">formation</span>
+          </h2>
+        
+        </div>
       <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-4">
         {formations.map((formation, index) => (
           <Link 
